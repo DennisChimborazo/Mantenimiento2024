2049095da5584aedaecb62d6132e78c1021433c5
DennisChimborazo <ddcdalex@gmail.com>
2024-12-13 15:38:45 -0500

    Mensajes de verificacion de datos en activos

--- frontend/src/assets/components/compra/NuevaProCompraFun.js
+++ frontend/src/assets/components/compra/NuevaProCompraFun.js
@@ -1,4 +1,5 @@
 import ApiService from "../../Services/ApiMetodos.js"; // Asegúrate de que la URL esté correcta
+import mostrarMensaje from "../Mensajes/Mensaje.js";
 
 export const cargarProveedores = async () => {
         const data = await ApiService.traerDatos("proovedor");
@@ -9,6 +10,12 @@ export const guardarProcesoCompra = async (form) => {
     try {
         const resultado = await ApiService.enviarDatos("proccompra", form);
         if (resultado) {
+          mostrarMensaje({
+            title: "Se guardo",
+            text: "Se creo un nuevo proceso de compra",
+            icon: "success",
+            timer: 2500
+          });
           console.log("Proceso de compra guardado con éxito:", resultado);
         } else {
           console.log("Hubo un problema al guardar el proceso de compra.");
